147c3bacd7e5a19a82f11d3ccd84883f63fbdbf0
linus-rudbeck <linus.rudbeck@distansakademin.se>
2023-09-20 11:39:14 +0200

    Initiate project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# code-pipeline-demo
+# Code pipeline demo
+
+[Link to guide](https://enlear.academy/deploy-your-spring-boot-application-using-codedeploy-and-codepipeline-4d853b1e486e)
---
+++ src/main/java/se/distansakademin/codepipelinedemo/CodePipelineDemoApplication.java
@@ -0,0 +1,13 @@
+package se.distansakademin.codepipelinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePipelineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodePipelineDemoApplication.class, args);
+	}
+
+}
