b3a0f786458ea071991711cbb1435a86a6e57f89
linus-rudbeck <linus.rudbeck@distansakademin.se>
2023-09-27 15:40:51 +0200

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar -Dserver.port=443 \
-    -Dserver.ssl.key-store=letsencrypt.jks  \
-    -Dserver.ssl.key-store-password=password  \
-    -Dserver.ssl.key-alias=simple-cert  \
-    -Dserver.ssl.key-password=password  \
+sudo java -jar -Dserver.port=80 \
     *.jar > /dev/null 2> /dev/null < /dev/null &
