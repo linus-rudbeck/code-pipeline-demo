f229049a3b9f26b65d882f830139fbbff1d55efc
linus-rudbeck <linus.rudbeck@distansakademin.se>
2023-09-27 15:34:06 +0200

    Added https

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar -Dserver.port=80 \
+sudo java -jar -Dserver.port=443 \
+    -Dserver.ssl.key-store=letsencrypt.jks  \
+    -Dserver.ssl.key-store-password=password  \
+    -Dserver.ssl.key-alias=simple-cert  \
+    -Dserver.ssl.key-password=password  \
     *.jar > /dev/null 2> /dev/null < /dev/null &
